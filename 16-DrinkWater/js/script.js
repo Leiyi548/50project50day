1084c41e29dc0ab72fb24e8a7b101d12993b25c6
Leiyi548 <1424630446@qq.com>
2022-06-23 13:20:08 +0800

    :sparkles: 提交以前未提交的

---
+++ 16-DrinkWater/js/script.js
@@ -0,0 +1,43 @@
+// get cups element
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+let index = 0
+smallCups.forEach((cup) => {
+  cup.addEventListener('click', () => {
+    if (cup.classList.contains('full')) {
+      cup.classList.remove('full')
+      index--
+    } else {
+      cup.classList.add('full')
+      index++
+    }
+    liters.innerText = 250 * index + 'L'
+    updateBigCup()
+  })
+})
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.innerText = ''
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden'
+    remained.style.height = 0
+  } else {
+    remained.style.visibility = 'visible'
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`
+  }
+}
